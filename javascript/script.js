d25d7f79686664c19a0fcdbf61f4a460e3796cdd
RiccardoMarroni <marroni.riccardo@gmail.com>
2024-03-20 13:54:44 +0100

    esercizio da sistemare

--- javascript/script.js
+++ javascript/script.js
@@ -18,7 +18,7 @@ numbers.innerHTML = randomNumbersArray;
 // faccio il clock
 
 const timeIsUp = document.getElementById('clock');
-let countdown = 31;
+let countdown = 5;
 const timerCountdown = setInterval(function () {
     countdown--;
     if (countdown === 0) {
@@ -28,5 +28,34 @@ const timerCountdown = setInterval(function () {
 }, 1000);
 const waitingTime1 = setTimeout(deleteNumbers, 31000, numbers);
 
+
+// numeri utente
+
 const guessedNumbers = [];
 const userNumberArray = [];
+
+
+// funzione cercata online
+const waitingTime2 = setTimeout(createPrompts, 32000, 5, userNumberArray);
+
+
+function deleteNumbers(domElement) {
+    domElement.innerHTML = 'Te li ricordi?';
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+function createPrompts(iterations, array) {
+    for (let i = 0; i < iterations; i++) {
+        let userNumber = parseInt(prompt('Inserisci uno dei numeri'));
+        array.push(userNumber);
+    }
+    for (let i = 0; i < randomNumbersArray.length; i++) {
+        if (userNumberArray.includes(randomNumbersArray[i])) {
+            guessedNumbers.push(randomNumbersArray[i]);
+        }
+    }
+    numbers.innerHTML = 'Hai indovinato ' + guessedNumbers.length + ' numeri: ' + guessedNumbers;
+}
