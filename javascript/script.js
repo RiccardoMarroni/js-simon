4069db94a80db6ac09129e90bbb4dcda3b6aada2
RiccardoMarroni <marroni.riccardo@gmail.com>
2024-03-20 13:46:23 +0100

    clock

--- javascript/script.js
+++ javascript/script.js
@@ -4,9 +4,29 @@
  const randomNumbersArray = [];
  let randomNumbers;
 
-for(i = 0; i < 6; i++){
+for(i = 0; i < 5; i++){
     let i = parseInt(Math.random() * (100 - 1) + 1)
     randomNumbersArray.push(i);
 }
 
 console.log(randomNumbersArray)
+
+
+const numbers = document.getElementById('numbers');
+numbers.innerHTML = randomNumbersArray;
+
+// faccio il clock
+
+const timeIsUp = document.getElementById('clock');
+let countdown = 31;
+const timerCountdown = setInterval(function () {
+    countdown--;
+    if (countdown === 0) {
+        clearInterval(timerCountdown);
+    }
+    timeIsUp.innerHTML = countdown;
+}, 1000);
+const waitingTime1 = setTimeout(deleteNumbers, 31000, numbers);
+
+const guessedNumbers = [];
+const userNumberArray = [];
